b7ffc2f24b6c0142367d40be52f48c6f253d6b27
915marco <915marco@gmail.com>
2022-06-16 22:37:04 -0500

    questions for readme added

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,98 @@
 // TODO: Include packages needed for this application
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        name: 'title',
+        message: "Enter a title for the Project",
+        validate: titleInput => {
+            if(titleInput){
+                return true;
+            } else {
+                alert("Please Enter a title for the Project.");
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: " Please enter a description for the Project.",
+    },
+
+    {
+        type: 'input',
+        name: 'Github id',
+        message: "Please Enter your GitHub ID : ",
+        validate: githubIdInput => {
+            if(githubIdInput){
+                return true;
+            } else {
+                alert(" Please enter your GitHub ID");
+                return false;
+            } 
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: "To install the application",
+        default: 'npm install'
+    },
+
+    {
+     type: 'input',
+     name: 'usage',
+     message: "Application used to install a professional readme following a few input prompts."   
+    },
+
+    {
+        type: 'input',
+        name: 'license',
+        message: "What license would you like to use for this project?",
+        choices: ['GNU GPLv3', 'BSD3', 'MIT', 'Mozilla public license 3'],
+        default: 'GNU GPLv3'
+    
+    },
+
+    {
+        type: 'input',
+        name: 'contribution',
+        message: "Will anyone be contributing to this project with you, if so please enter their github Id's.",
+        default: false 
+    },
+
+    {
+        type: 'input',
+        name: 'tests',
+        message: "to test the application run a command of 'npm test' ",
+        default: "npm test"
+    },
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        const readmeData = generateMarkdown(answers);
+        fs.writeFile('./README.md', readmeData, err => {
+            if(err) throw new Error(err);
+            console.log('');
+        });
+    });
+}
 
 // Function call to initialize app
 init();
